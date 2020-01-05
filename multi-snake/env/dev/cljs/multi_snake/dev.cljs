d7eeefb6cc0d5bb1b78d33dcdb30f7abbdb54b83
danielhvs <danielhvs70@gmail.com>
2020-01-04 21:03:21 -0300

    criando multi-snake e movendo codigo js para diretorio "original" para referencia

---
+++ multi-snake/env/dev/cljs/multi_snake/dev.cljs
@@ -0,0 +1,15 @@
+(ns ^:figwheel-no-load multi-snake.dev
+  (:require
+    [multi-snake.core :as core]
+    [devtools.core :as devtools]))
+
+(extend-protocol IPrintWithWriter
+  js/Symbol
+  (-pr-writer [sym writer _]
+    (-write writer (str "\"" (.toString sym) "\""))))
+
+(devtools/install!)
+
+(enable-console-print!)
+
+(core/init!)
