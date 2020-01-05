bffd914d073ade17aca8e3f09cf2830a98985857
danielhvs <danielhvs70@gmail.com>
2020-01-04 22:27:12 -0300

    correcao hot-reload

--- multi-snake/src/cljs/multi_snake/core.cljs
+++ multi-snake/src/cljs/multi_snake/core.cljs
@@ -34,6 +34,11 @@
 
 ;; -------------------------
 ;; Initialize app
-(defn init! []
+(defn mount-root []
   (reagent/render [home-page] 
                   (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
+
+(mount-root)
