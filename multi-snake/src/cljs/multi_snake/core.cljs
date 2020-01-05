878cafb10a61a10fbdb384127fb8a80371e5abf9
danielhvs <danielhvs70@gmail.com>
2020-01-04 21:39:49 -0300

    desenhando quadrados na tela com svg

--- multi-snake/src/cljs/multi_snake/core.cljs
+++ multi-snake/src/cljs/multi_snake/core.cljs
@@ -3,11 +3,32 @@
    [reagent.core :as reagent :refer [atom]]
 ))
 
-;; -------------------------
-;; Page components
-(defn home-page []
-  (fn []
-    [:h1 "Welcome to multi-snake"]))
+(def scenario1
+  [{:x 0 :y 0} {:x 1 :y 1} {:x 0 :y 1} {:x 1 :y 0}])
+
+(defonce app-state (atom {:size 40 :scenario scenario1}))
+
+(defn blank [p]
+  [:rect
+   {:width 0.95
+    :height 0.95
+    :fill "green"
+    :x (+ 0.05 (:x p))
+    :y (+ 0.05 (:y p))}])
+
+(defn home-page []  
+  [:div
+   [:div
+    [:center
+     (let [width 2
+           height 2
+           ]
+       (into [:svg
+              {:view-box (str "0 0 " width " " height)
+               :width (* (:size @app-state) width)
+               :preserveAspectRatio "xMinYMin meet"
+               :height (* (:size @app-state) height)}]
+             (map blank (:scenario @app-state))))]]])
 
 ;; -------------------------
 ;; Initialize app
